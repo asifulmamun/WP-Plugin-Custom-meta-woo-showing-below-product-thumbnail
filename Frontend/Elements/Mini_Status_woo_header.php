aaaabe63be6807208a85024e93a6f39cd0fa8d41
asifulmamun <asifulmamun@gmail.com>
2024-03-18 22:08:19 +0600

    feature: add icon

--- Frontend/Elements/Mini_Status_woo_header.php
+++ Frontend/Elements/Mini_Status_woo_header.php
@@ -16,7 +16,12 @@ if(!class_exists('Mini_Status_woo_header')){
             <section class="container product_mini_status">
                 <ul class="row">
                     <li class="col-6 col-lg-3 col-md-3">
-                        <img src="https://i.postimg.cc/MTYbBMPP/Screenshot-from-2024-03-15-00-28-42.png" alt="Auto Update">
+                        <!-- <img src="https://i.postimg.cc/MTYbBMPP/Screenshot-from-2024-03-15-00-28-42.png" alt="Auto Update"> -->
+                        <span class="mini_status_ic">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                            </svg>
+                        </span>
                         <div class="mini_status_desc">
                             <div>Auto Update</div>
                             <div>
@@ -49,7 +54,12 @@ if(!class_exists('Mini_Status_woo_header')){
                         </div>
                     </li>
                     <li class="col-6 col-lg-3 col-md-3">
-                        <img src="https://i.postimg.cc/MTYbBMPP/Screenshot-from-2024-03-15-00-28-42.png" alt="Auto Update">
+                        <!-- <img src="https://i.postimg.cc/MTYbBMPP/Screenshot-from-2024-03-15-00-28-42.png" alt="Auto Update"> -->
+                        <span class="mini_status_ic">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </svg>
+                        </span>
                         <div class="mini_status_desc">
                             <div>Mannual Update</div>
                             <div>
@@ -83,7 +93,12 @@ if(!class_exists('Mini_Status_woo_header')){
                     </li>
 
                     <li class="col-6 col-lg-3 col-md-3">
-                        <img src="https://i.postimg.cc/hj38MWnT/Screenshot-from-2024-03-15-00-44-35.png" alt="Auto Update">
+                        <!-- <img src="https://i.postimg.cc/hj38MWnT/Screenshot-from-2024-03-15-00-44-35.png" alt="Auto Update"> -->
+                        <span class="mini_status_ic">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+                            </svg>
+                        </span>
                         <div class="mini_status_desc">
                             <div>Version</div>
                             <div>
@@ -95,7 +110,12 @@ if(!class_exists('Mini_Status_woo_header')){
                     </li>
                     
                     <li class="col-6 col-lg-3 col-md-3">
-                        <img src="https://i.postimg.cc/pV4YFHqt/Screenshot-from-2024-03-15-00-45-15.png" alt="Auto Update">
+                        <!-- <img src="https://i.postimg.cc/pV4YFHqt/Screenshot-from-2024-03-15-00-45-15.png" alt="Auto Update"> -->
+                        <span class="mini_status_ic">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                            </svg>
+                        </span>
                         <div class="mini_status_desc">
                             <div>Update On</div>
                             <div>
@@ -120,6 +140,17 @@ if(!class_exists('Mini_Status_woo_header')){
                     display: flex;
                     justify-content: center;
                     align-items: center;
+                }
+                
+                span.mini_status_ic{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    padding-right: 1rem;
+                }
+                span.mini_status_ic svg{
+                    color: var(--custom_secondary_color);
+                    width: 2.4rem;
                 }
 
                 .product_mini_status .mini_status_desc div:first-child{
--- Frontend/Elements/Virus_total_el_single_product.php
+++ Frontend/Elements/Virus_total_el_single_product.php
@@ -10,11 +10,16 @@
             public static function virus_status_btn_before_add_to_cart_single_page(){
 
                 // Virus Total Status Show if Hash file exist
-                echo '<a class="vt_status_check" style="cursor:pointer;" href="#virus_total_full_status_wrap">Files Verified by VirusTotal!</a>'; ?>
+                echo '<a class="vt_status_check" style="cursor:pointer;" href="#virus_total_full_status_wrap"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" /></svg>&nbsp;Files Verified by VirusTotal!
+                </a>'; ?>
 
                 <style>
                     a.vt_status_check{
                         color: var(--virus_total_primary_color);
+                        display: flex;
+                    }
+                    a.vt_status_check svg{
+                        width: 1.5rem;
                     }
                 </style>
 
@@ -24,12 +29,15 @@
             // After product desc/summery
             public static function after_pdocut_summery_or_desc($data){?>
                 <div id="virus_total_full_status_wrap">
-                    <a class="btn btn-primary" target="_blank" href="https://www.virustotal.com/gui/search/<?php echo $data; ?>">Check Virus Live</a>
+                    <a class="btn btn-primary" target="_blank" href="https://www.virustotal.com/gui/search/<?php echo $data; ?>">Check Virus Status Live</a>
                 </div>
                 <style>
                     #virus_total_full_status_wrap a{
                         background-color: var(--virus_total_primary_color);
                         color: white;
+                    }
+                    #virus_total_full_status_wrap svg{
+                        color: var(--virus_total_primary_color);
                     }
                 </style>
             <?php }// After product desc/summery
