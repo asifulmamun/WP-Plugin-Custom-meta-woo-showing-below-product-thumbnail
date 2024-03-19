68c38217af7846046f4374f59d2c2c085fa4d961
asifulmamun <asifulmamun@gmail.com>
2024-03-19 13:52:46 +0600

    v2.0.0 - feature add and breadk(v1): new version

---
+++ README.md
@@ -0,0 +1,8 @@
+#### ChangeLog
+
+v2.0.0-beta     : Add field (Auto Update, Mannual Update, Version, Update On, Virus Total Hash File)
+                  Custom Header, Mini status with new custom fields - Single Product Page
+                  Check Virus Total Status by Link if filled hash number while product add/edit - Single Product Page
+
+v1.5.3          : View Demo and Watch Video Dynamic Editable while link add also if not set automatic Default View Demo and Wathc Video btn show if data filled
+v1.0.0-alpha    : View Demo and Watch Video Link custom field if filled data show
--- boot.php
+++ boot.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: wp-premium.org
-Plugin URI: https://github.com/asifulmamun/WooCommerce-wp-premium.org
+Plugin URI: https://github.com/asifulmamun/wp-premium.org
 Description: This is the custom meta box, where user can add meta from product edit page and it will be showing below the product thumbnail, this plugin inspiration by https://wp-premium.org
 Version: 2.0.0
 Author: Al Mamun - asifulmamun
@@ -74,7 +74,7 @@ register_deactivation_hook(__FILE__, 'deactive');
         // btn
         $demo->meta_name_btn_txt = 'Button Name';
         $demo->field_name_btn_submit = 'demo_btn';
-        $demo->field_name_btn_txt = 'Sales Page';
+        $demo->field_name_btn_txt = 'View Demo';
         // Show
         use Frontend\Custom_btn as Demo_btn;
         $demo_btn = new Demo_btn;
